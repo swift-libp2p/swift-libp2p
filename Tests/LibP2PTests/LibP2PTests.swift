50c9bf8da054d1111da0a4877729d1d275147775
Brandon <32753167+btoms20@users.noreply.github.com>
2025-11-14 11:37:06 -0800

    migrated to swift-testing and added async tests

--- Tests/LibP2PTests/LibP2PTests.swift
+++ Tests/LibP2PTests/LibP2PTests.swift
@@ -12,26 +12,107 @@
 //
 //===----------------------------------------------------------------------===//
 
-import XCTest
+import Testing
 
 @testable import LibP2P
 
-final class LibP2PTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        //XCTAssertEqual(swift_libp2p().text, "Hello, World!")
-    }
+@Suite("Libp2p Tests", .serialized)
+struct LibP2PTests {
 
-    func testLibP2P() throws {
+    @Test func testLibP2P() throws {
         let app = try Application(.detect())
         defer { app.shutdown() }
 
+        // .detect() should result in .testing
+        #expect(app.environment == Environment.testing)
+        
         try app.start()
 
-        sleep(3)
+        sleep(1)
+    }
+    
+    @Test func testLibP2P_Development_Environment() throws {
+        let app = Application(.development)
+        defer { app.shutdown() }
 
-        app.running?.stop()
+        #expect(app.environment == Environment.development)
+        
+        try app.start()
+
+        sleep(1)
+    }
+
+    @Test func testLibP2P_Async() async throws {
+        let app = Application(.testing)
+        
+        #expect(app.environment == Environment.testing)
+        
+        try await app.startup()
+
+        try await Task.sleep(for: .seconds(1))
+
+        try await app.asyncShutdown()
+    }
+
+    @Test func testLibP2PRoutes_Default_Identify_Routes() throws {
+        let app = try Application(.detect())
+
+        try app.start()
+
+        #expect(
+            app.routes.all.map { $0.description } == [
+                "/ipfs/id/1.0.0", "/ipfs/id/push/1.0.0", "/ipfs/ping/1.0.0", "/p2p/id/delta/1.0.0",
+            ]
+        )
+
+        sleep(1)
+
+        app.shutdown()
+    }
+
+    @Test func testLibP2PRoutes_Default_Identify_Routes_Async() async throws {
+        let app = try Application(.detect())
+
+        try await app.startup()
+
+        #expect(
+            app.routes.all.map { $0.description } == [
+                "/ipfs/id/1.0.0", "/ipfs/id/push/1.0.0", "/ipfs/ping/1.0.0", "/p2p/id/delta/1.0.0",
+            ]
+        )
+
+        try await Task.sleep(for: .seconds(1))
+
+        try await app.asyncShutdown()
+    }
+
+    @Test func testLibP2PRoutes_Additional_Routes_Async() async throws {
+        let app = try Application(.detect())
+
+        app.routes.group("api") { api in
+            // Ensure that we can register non-async routes
+            api.on("nonAsyncEcho") { req -> Response<ByteBuffer> in
+                .respondThenClose(req.payload)
+            }
+
+            // Ensure that we can register async routes
+            api.on("asyncEcho") { req -> Response<ByteBuffer> in
+                try await Task.sleep(for: .seconds(1))
+                return .respondThenClose(req.payload)
+            }
+        }
+
+        try await app.startup()
+
+        #expect(
+            app.routes.all.map { $0.description } == [
+                "/ipfs/id/1.0.0", "/ipfs/id/push/1.0.0", "/ipfs/ping/1.0.0", "/p2p/id/delta/1.0.0",
+                "/api/nonAsyncEcho", "/api/asyncEcho",
+            ]
+        )
+
+        try await Task.sleep(for: .seconds(1))
+
+        try await app.asyncShutdown()
     }
 }
