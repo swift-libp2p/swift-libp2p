8e9ea8bcf86ef9882ddd3e09b75f8627359efffa
Brandon <32753167+btoms20@users.noreply.github.com>
2025-11-14 10:56:21 -0800

    bumped dependencies

--- Package.swift
+++ Package.swift
@@ -31,61 +31,56 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // Swift NIO for all things networking
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
-        // 💻 APIs for creating interactive CLI tools.
-        .package(url: "https://github.com/vapor/console-kit.git", from: "4.0.0"),
-        // Logging
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        // 💥 Backtraces for Swift on Linux
-        .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.1.1"),
-        // 🚍 High-performance trie-node router.
-        .package(url: "https://github.com/vapor/routing-kit.git", from: "4.0.0"),
         // LibP2P Core
-        .package(url: "https://github.com/swift-libp2p/swift-libp2p-core.git", .upToNextMinor(from: "0.3.0")),
-        // Multiaddr
-        .package(url: "https://github.com/swift-libp2p/swift-multiaddr.git", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-libp2p-core.git", .upToNextMinor(from: "0.4.0")),
+        // LibP2P Multiaddr
+        .package(url: "https://github.com/swift-libp2p/swift-multiaddr.git", .upToNextMinor(from: "0.2.0")),
         // LibP2P Peer Identities
-        .package(url: "https://github.com/swift-libp2p/swift-peer-id.git", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-peer-id.git", .upToNextMinor(from: "0.2.0")),
+        // Swift NIO for all things networking
+        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.87.0")),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", .upToNextMajor(from: "1.25.0")),
+        // 💻 APIs for creating interactive CLI tools.
+        .package(url: "https://github.com/vapor/console-kit.git", .upToNextMajor(from: "4.0.0")),
+        // 🚍 High-performance trie-node router.
+        .package(url: "https://github.com/vapor/routing-kit.git", .upToNextMajor(from: "4.0.0")),
+        // Logging
+        .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.6.0")),
         // Sugary extensions for the SwiftNIO library
-        .package(url: "https://github.com/vapor/async-kit.git", .exact("1.11.1")),
+        .package(url: "https://github.com/vapor/async-kit.git", .upToNextMajor(from: "1.11.1")),
         // Swift Protobuf
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .exact("1.19.0")),
+        .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.19.0")),
         // Swift metrics API
-        .package(url: "https://github.com/apple/swift-metrics.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-metrics.git", .upToNextMajor(from: "2.5.0")),
         // Swift Event Bus
-        .package(url: "https://github.com/cesarferreira/SwiftEventBus.git", from: "5.0.0"),
+        .package(url: "https://github.com/cesarferreira/SwiftEventBus.git", .upToNextMajor(from: "5.0.0")),
         // SwiftState for state machines
-        .package(url: "https://github.com/ReactKit/SwiftState.git", from: "6.0.0"),
+        .package(url: "https://github.com/ReactKit/SwiftState.git", .upToNextMajor(from: "6.0.0")),
     ],
     targets: [
         // C
-        //.target(name: "CBase32"),
-        //.target(name: "CBcrypt"),
         .target(name: "COperatingSystem"),
-        //.target(name: "CURLParser"),
 
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LibP2P",
             dependencies: [
+                .product(name: "LibP2PCore", package: "swift-libp2p-core"),
+                .product(name: "Multiaddr", package: "swift-multiaddr"),
+                .product(name: "PeerID", package: "swift-peer-id"),
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
                 .product(name: "NIOExtras", package: "swift-nio-extras"),
                 .product(name: "ConsoleKit", package: "console-kit"),
                 .product(name: "Logging", package: "swift-log"),
-                .product(name: "Backtrace", package: "swift-backtrace"),
-                .product(name: "LibP2PCore", package: "swift-libp2p-core"),
                 .product(name: "RoutingKit", package: "routing-kit"),
-                .product(name: "Multiaddr", package: "swift-multiaddr"),
-                .product(name: "PeerID", package: "swift-peer-id"),
                 .product(name: "AsyncKit", package: "async-kit"),
-                .product(name: "SwiftProtobuf", package: "SwiftProtobuf"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
                 .product(name: "Metrics", package: "swift-metrics"),
                 .product(name: "SwiftEventBus", package: "SwiftEventBus"),
                 .product(name: "SwiftState", package: "SwiftState"),
+                .product(name: "_NIOFileSystem", package: "swift-nio"),
                 .target(name: "COperatingSystem"),
             ]
         ),
