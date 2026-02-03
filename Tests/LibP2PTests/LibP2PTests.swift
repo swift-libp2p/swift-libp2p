4b388d89c1913eb11799e24c063bb7a3edf2481b
Brandon <32753167+btoms20@users.noreply.github.com>
2026-02-03 14:30:23 -0800

    added tests, refactored tests into their own files

--- Tests/LibP2PTests/LibP2PTests.swift
+++ Tests/LibP2PTests/LibP2PTests.swift
@@ -12,26 +12,43 @@
 //
 //===----------------------------------------------------------------------===//
 
-import Multiaddr
+import LibP2PTesting
 import Testing
-
-@testable import LibP2P
 
 @Suite("Libp2p Tests", .serialized)
 struct LibP2PTests {
 
+    @available(*, deprecated, message: "Transition to async tests")
     @Test func testLibP2P() throws {
         let app = try Application(.detect())
         defer { app.shutdown() }
 
         // .detect() should result in .testing
         #expect(app.environment == Environment.testing)
+        #expect(app.logger.label.hasPrefix("libp2p.application"))
+        #expect(app.logger.label.count == 26)
 
         try app.start()
 
         usleep(10_000)
     }
 
+    @available(*, deprecated, message: "Transition to async tests")
+    @Test func testLibP2P_CustomLogger() throws {
+        let logger = Logger(label: "custom")
+        let app = try Application(.detect(), logger: logger)
+        defer { app.shutdown() }
+
+        // .detect() should result in .testing
+        #expect(app.environment == Environment.testing)
+        #expect(app.logger.label == "custom")
+
+        try app.start()
+
+        usleep(10_000)
+    }
+
+    @available(*, deprecated, message: "Transition to async tests")
     @Test func testLibP2P_Development_Environment() throws {
         let app = Application(.development)
         defer { app.shutdown() }
@@ -44,9 +61,25 @@ struct LibP2PTests {
     }
 
     @Test func testLibP2P_Async() async throws {
-        let app = try await Application.make(.testing, peerID: .ephemeral())
+        let app = try await Application.make(.testing, peerID: .ephemeral)
 
         #expect(app.environment == Environment.testing)
+        #expect(app.logger.label.hasPrefix("libp2p.application"))
+        #expect(app.logger.label.count == 26)
+
+        try await app.startup()
+
+        try await Task.sleep(for: .milliseconds(10))
+
+        try await app.asyncShutdown()
+    }
+
+    @Test func testLibP2P_Async_CustomLogger() async throws {
+        let logger = Logger(label: "custom")
+        let app = try await Application.make(.testing, peerID: .ephemeral, logger: logger)
+
+        #expect(app.environment == Environment.testing)
+        #expect(app.logger.label == "custom")
 
         try await app.startup()
 
@@ -56,7 +89,7 @@ struct LibP2PTests {
     }
 
     @Test func testLibP2P_Async_ListeningAddress() async throws {
-        let app = try await Application.make(.testing, peerID: .ephemeral())
+        let app = try await Application.make(.testing, peerID: .ephemeral)
 
         #expect(app.environment == Environment.testing)
 
@@ -71,65 +104,148 @@ struct LibP2PTests {
         try await app.asyncShutdown()
     }
 
-    @Test func testLibP2PRoutes_Default_Identify_Routes() throws {
-        let app = try Application(.detect())
-
-        try app.start()
-
-        #expect(
-            app.routes.all.map { $0.description } == [
-                "/ipfs/id/1.0.0", "/ipfs/id/push/1.0.0", "/ipfs/ping/1.0.0", "/p2p/id/delta/1.0.0",
-            ]
-        )
-
-        usleep(10_000)
-
-        app.shutdown()
+    @Test func testWithApp() async throws {
+        try await withApp { app in
+            #expect(app.environment == Environment.testing)
+            #expect(app.peerID.type == .isPrivate)
+            #expect(app.peerID.keyPair?.keyType == .ed25519)
+            #expect(app.listenAddresses.isEmpty)
+            #expect(app.logger.label.hasPrefix("libp2p.application"))
+            #expect(app.logger.label.count == 26)
+        }
     }
 
-    @Test func testLibP2PRoutes_Default_Identify_Routes_Async() async throws {
-        let app = try await Application.make(.detect(), peerID: .ephemeral())
+    @Test func testWithApp_ShutdownAfterError() async throws {
+        // An external reference to our app
+        var appReference: Application? = nil
+        // Catch our errors to prevent early termination
+        do {
+            // Instantiate our app
+            try await withApp { app in
+                // Store a reference to our app (so we can ensure shutdown after the error)
+                appReference = app
+                #expect(app.environment == Environment.testing)
+                #expect(app.peerID.type == .isPrivate)
+                #expect(app.peerID.keyPair?.keyType == .ed25519)
+                #expect(app.listenAddresses.isEmpty)
 
-        try await app.startup()
-
-        #expect(
-            app.routes.all.map { $0.description } == [
-                "/ipfs/id/1.0.0", "/ipfs/id/push/1.0.0", "/ipfs/ping/1.0.0", "/p2p/id/delta/1.0.0",
-            ]
-        )
-
-        try await Task.sleep(for: .milliseconds(10))
-
-        try await app.asyncShutdown()
+                // This will throw an error (MultiaddrError.invalidFormat)
+                let _ = try await app.resolve(Multiaddr("")).get()
+            }
+        } catch let maError as MultiaddrError {
+            #expect(maError == .invalidFormat)
+        } catch {
+            Issue.record(error)
+        }
+        // Ensure that we have a reference to the app
+        let ref = try #require(appReference)
+        // And that it properly shutdown after the error occured
+        #expect(ref.isRunning == false)
+        #expect(ref.didShutdown == true)
     }
 
-    @Test func testLibP2PRoutes_Additional_Routes_Async() async throws {
-        let app = try await Application.make(.detect(), peerID: .ephemeral())
+    @Test(.disabled(), .bug("https://github.com/swift-libp2p/swift-libp2p/issues/45"))
+    func testAutomaticPortPickingWorks() async throws {
+        try await withApp(configure: { app in
+            app.listen(.tcp(host: "127.0.0.1", port: 0))
 
-        app.routes.group("api") { api in
-            // Ensure that we can register non-async routes
-            api.on("nonAsyncEcho") { req -> Response<ByteBuffer> in
-                .respondThenClose(req.payload)
+            app.on("hello") { req in
+                "Hello, world!"
             }
 
-            // Ensure that we can register async routes
-            api.on("asyncEcho") { req -> Response<ByteBuffer> in
-                try await Task.sleep(for: .seconds(1))
-                return .respondThenClose(req.payload)
+            #expect(app.servers.server(for: TCPServer.self)?.localAddress == nil)
+
+            app.environment.arguments = ["serve"]
+
+        }) { app in
+            let localAddress = try #require(app.servers.server(for: TCPServer.self)?.listeningAddress)
+            guard let tcp = localAddress.tcpAddress else {
+                Issue.record("couldn't get ip/port from `\(localAddress)`")
+                return
             }
+
+            #expect(tcp.address == "127.0.0.1")
+            #expect(tcp.port > 0)
+        }
+    }
+
+    @Test(.disabled(), .bug("https://github.com/swift-libp2p/swift-libp2p/issues/45"))
+    func testConfigurationAddressDetailsReflectedAfterBeingSet() async throws {
+        struct AddressConfig: Codable {
+            let hostname: String
+            let port: Int
         }
 
-        try await app.startup()
+        try await withApp(configure: { app in
+            app.servers.use(.tcp(host: "0.0.0.0", port: 0))
 
-        #expect(
-            app.routes.all.map { $0.description } == [
-                "/ipfs/id/1.0.0", "/ipfs/id/push/1.0.0", "/ipfs/ping/1.0.0", "/p2p/id/delta/1.0.0",
-                "/api/nonAsyncEcho", "/api/asyncEcho",
-            ]
-        )
+            app.on("hello") { req -> Response<ByteBuffer> in
+                let serverConf = try #require(
+                    req.application.servers.server(for: TCPServer.self)?.listeningAddress.tcpAddress
+                )
+                let config = AddressConfig(hostname: serverConf.address, port: serverConf.port)
+                let buffer = try ByteBuffer(bytes: JSONEncoder().encode(config))
+                return .respondThenClose(buffer)
+            }
 
-        try await Task.sleep(for: .milliseconds(10))
+            app.environment.arguments = ["serve"]
+        }) { app in
+            let localAddress = try #require(app.servers.server(for: TCPServer.self)?.listeningAddress)
+            //#expect("0.0.0.0" == app.servers.server(forKey: TCPServer.key).configuration.hostname)
+            //#expect(app.http.server.shared.localAddress?.port == app.http.server.configuration.port)
 
-        try await app.asyncShutdown()
+            guard let tcp = localAddress.tcpAddress else {
+                Issue.record("couldn't get ip/port from `\(localAddress)`")
+                return
+            }
+
+            print(tcp)
+
+            //let response = try await app.testing().test("http://localhost:\(port)/hello")
+            //let returnedConfig = try response.content.decode(AddressConfig.self)
+            //#expect(returnedConfig.hostname == "0.0.0.0")
+            //#expect(returnedConfig.port == port)
+        }
+    }
+
+    @Test(.disabled(), .bug("https://github.com/swift-libp2p/swift-libp2p/issues/45"))
+    func testConfigurationAddressDetailsReflectedWhenProvidedThroughServeCommand() async throws {
+        struct AddressConfig: Codable {
+            let hostname: String
+            let port: Int
+        }
+
+        try await withApp(configure: { app in
+            app.servers.use(.tcp(host: "0.0.0.0", port: 3000))
+
+            app.on("hello") { req -> Response<ByteBuffer> in
+                let serverConf = try #require(
+                    req.application.servers.server(for: TCPServer.self)?.listeningAddress.tcpAddress
+                )
+                let config = AddressConfig(hostname: serverConf.address, port: serverConf.port)
+                let buffer = try ByteBuffer(bytes: JSONEncoder().encode(config))
+                return .respondThenClose(buffer)
+            }
+
+            app.environment.arguments = ["serve", "--hostname", "0.0.0.0", "--port", "3000"]
+        }) { app in
+            //XCTAssertNotNil(app.http.server.shared.localAddress)
+            //XCTAssertEqual("0.0.0.0", app.http.server.configuration.hostname)
+            //XCTAssertEqual(3000, app.http.server.configuration.port)
+            let localAddress = try #require(app.servers.server(for: TCPServer.self)?.listeningAddress)
+            print(app.listenAddresses)
+            guard let tcp = localAddress.tcpAddress else {
+                Issue.record("couldn't get ip/port from `\(localAddress)`")
+                return
+            }
+
+            #expect(tcp.address == "0.0.0.0")
+            #expect(tcp.port == 3000)
+
+            //let response = try app.client.get("http://localhost:\(port)/hello").wait()
+            //let returnedConfig = try response.content.decode(AddressConfig.self)
+            //#expect(returnedConfig.hostname, "0.0.0.0")
+            //#expect(returnedConfig.port, 3000)
+        }
     }
 }
