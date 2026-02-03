805f13b527ddca55afa67fd9c2b3d457a22489ed
Brandon <32753167+btoms20@users.noreply.github.com>
2026-02-03 14:27:13 -0800

    added test helpers when using swift-testing

---
+++ Sources/LibP2PTesting/Exports.swift
@@ -0,0 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libp2p open source project
+//
+// Copyright (c) 2022-2025 swift-libp2p project authors
+// Licensed under MIT
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of swift-libp2p project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+
+@_documentation(visibility: internal) @_exported import LibP2P
+@_documentation(visibility: internal) @_exported import LibP2PTestUtils
---
+++ Sources/LibP2PTesting/TestingResponse.swift
@@ -0,0 +1,134 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libp2p open source project
+//
+// Copyright (c) 2022-2025 swift-libp2p project authors
+// Licensed under MIT
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of swift-libp2p project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+//
+//  Created by Vapor
+//  Modified by LibP2P on 1/29/26.
+//
+
+import Foundation
+import LibP2PTestUtils
+import Testing
+
+//public func expectContent<D>(
+//    _ type: D.Type,
+//    _ res: TestingResponse,
+//    fileID: String = #fileID,
+//    filePath: String = #filePath,
+//    line: Int = #line,
+//    column: Int = #column,
+//    _ closure: (D) throws -> Void
+//) rethrows where D: Decodable {
+//    LibP2PTestingContext.warnIfNotInSwiftTestingContext(
+//        fileID: fileID,
+//        filePath: filePath,
+//        line: line,
+//        column: column
+//    )
+//
+//    guard let contentType = res.headers.contentType else {
+//        let sourceLocation = Testing.SourceLocation(
+//            fileID: fileID,
+//            filePath: filePath,
+//            line: line,
+//            column: column
+//        )
+//        Issue.record("response does not contain content type", sourceLocation: sourceLocation)
+//        return
+//    }
+//
+//    let content: D
+//
+//    do {
+//        let decoder = try ContentConfiguration.global.requireDecoder(for: contentType)
+//        content = try decoder.decode(D.self, from: res.body, headers: res.headers)
+//    } catch {
+//        let sourceLocation = Testing.SourceLocation(
+//            fileID: fileID,
+//            filePath: filePath,
+//            line: line,
+//            column: column
+//        )
+//        Issue.record("could not decode body: \(error)", sourceLocation: sourceLocation)
+//        return
+//    }
+//
+//    try closure(content)
+//}
+
+public func expectContains(
+    _ haystack: String?,
+    _ needle: String?,
+    fileID: String = #fileID,
+    filePath: String = #filePath,
+    line: Int = #line,
+    column: Int = #column
+) {
+    LibP2PTestingContext.warnIfNotInSwiftTestingContext(
+        fileID: fileID,
+        filePath: filePath,
+        line: line,
+        column: column
+    )
+
+    let sourceLocation = Testing.SourceLocation(
+        fileID: fileID,
+        filePath: filePath,
+        line: line,
+        column: column
+    )
+    switch (haystack, needle) {
+    case (.some(let haystack), .some(let needle)):
+        #expect(haystack.contains(needle), "\(haystack) does not contain \(needle)", sourceLocation: sourceLocation)
+    case (.some(let haystack), .none):
+        Issue.record("\(haystack) does not contain nil", sourceLocation: sourceLocation)
+    case (.none, .some(let needle)):
+        Issue.record("nil does not contain \(needle)", sourceLocation: sourceLocation)
+    case (.none, .none):
+        Issue.record("nil does not contain nil", sourceLocation: sourceLocation)
+    }
+}
+
+public func expectJSONEquals<T>(
+    _ data: String?,
+    _ test: T,
+    fileID: String = #fileID,
+    filePath: String = #filePath,
+    line: Int = #line,
+    column: Int = #column
+)
+where T: Codable & Equatable {
+    LibP2PTestingContext.warnIfNotInSwiftTestingContext(
+        fileID: fileID,
+        filePath: filePath,
+        line: line,
+        column: column
+    )
+
+    let sourceLocation = Testing.SourceLocation(
+        fileID: fileID,
+        filePath: filePath,
+        line: line,
+        column: column
+    )
+    guard let data = data else {
+        Issue.record("nil does not equal \(test)", sourceLocation: sourceLocation)
+        return
+    }
+    do {
+        let decoded = try JSONDecoder().decode(T.self, from: Data(data.utf8))
+        #expect(decoded == test, sourceLocation: sourceLocation)
+    } catch {
+        Issue.record("could not decode \(T.self): \(error)", sourceLocation: sourceLocation)
+    }
+}
