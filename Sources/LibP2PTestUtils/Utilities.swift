2e2af5a4c14857a12a5dc538acc45f274f678406
Brandon <32753167+btoms20@users.noreply.github.com>
2026-02-03 14:24:37 -0800

    added a string method to ByteBuffer to make reading strings more convenient

---
+++ Sources/LibP2PTestUtils/Utilities.swift
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libp2p open source project
+//
+// Copyright (c) 2022-2025 swift-libp2p project authors
+// Licensed under MIT
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of swift-libp2p project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+//
+//  Created by Vapor
+//  Modified by LibP2P on 1/29/26.
+//
+
+import NIOCore
+
+extension ByteBuffer {
+    public var string: String {
+        .init(decoding: self.readableBytesView, as: UTF8.self)
+    }
+}
