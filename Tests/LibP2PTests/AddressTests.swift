b9c4dff6656681b32a8ed18d51b5be0e40b2a4a3
Brandon <btoms.20@gmail.com>
2026-08-19 20:55:23 -0700

    added tcpAddress tests to ensure our helper function parses dns and ip address when invoked

---
+++ Tests/LibP2PTests/AddressTests.swift
@@ -0,0 +1,38 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libp2p open source project
+//
+// Copyright (c) 2022-2025 swift-libp2p project authors
+// Licensed under MIT
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of swift-libp2p project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+
+import LibP2P
+import Testing
+
+extension LibP2PTests {
+    @Test("Ensure tcpAddress parses dns, dns4, dns6, ip4 and ip6 Multiaddr's")
+    func tcpAddressTests() throws {
+        let peer = "QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN"
+        let addresses = [
+            "/dns/sv15.bootstrap.libp2p.io/tcp/4001/p2p/\(peer)",
+            "/dns4/sv15.bootstrap.libp2p.io/tcp/4001/p2p/\(peer)",
+            "/dns6/sv15.bootstrap.libp2p.io/tcp/4001/p2p/\(peer)",
+            "/ip4/1.2.3.4/tcp/4001/p2p/\(peer)",
+            "/ip6/2604:1380:4602:5c00::3/tcp/4001/p2p/\(peer)"
+        ]
+        
+        for address in addresses {
+            #expect(throws: Never.self) {
+                let ma = try Multiaddr(address)
+                #expect(ma.tcpAddress != nil)
+                #expect(ma.tcpAddress?.port == 4001)
+            }
+        }
+    }
+}
