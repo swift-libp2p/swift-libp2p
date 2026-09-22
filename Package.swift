d134067bd04be3bdf615928bcc16717adaceb6dc
Brandon <btoms.20@gmail.com>
2026-09-22 08:56:08 -0700

    bump min macOS and iOS versions (unlocks Duration)

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ import PackageDescription
 let package = Package(
     name: "swift-libp2p",
     platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
+        .macOS(.v13),
+        .iOS(.v16),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
