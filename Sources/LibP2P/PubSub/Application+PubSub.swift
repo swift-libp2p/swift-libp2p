84a86042c334e381e06d15fab5ccb13faaf61e96
Brandon <btoms.20@gmail.com>
2026-09-23 11:07:25 -0700

    made PublishedResults sendable

--- Sources/LibP2P/PubSub/Application+PubSub.swift
+++ Sources/LibP2P/PubSub/Application+PubSub.swift
@@ -95,7 +95,7 @@ extension Application {
             print("----------------------------------")
         }
 
-        public enum PublishedResults {
+        public enum PublishedResults: Sendable {
             case failed(Error)
             case storedLocally
             case publishedToPeers(Int)
