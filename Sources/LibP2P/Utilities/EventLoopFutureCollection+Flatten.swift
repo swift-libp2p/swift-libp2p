4ef377ce82e6466ca63946719e55c48fbb0220e1
Brandon <btoms.20@gmail.com>
2026-09-23 17:41:31 -0700

    dropped AsyncKit, keep the flatten functions for the time being

---
+++ Sources/LibP2P/Utilities/EventLoopFutureCollection+Flatten.swift
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-libp2p open source project
+//
+// Copyright (c) 2022-2025 swift-libp2p project authors
+// Licensed under MIT
+//
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of swift-libp2p project authors
+//
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+
+import NIO
+
+/// AsyncKit's `flatten(on:)` collection helpers
+extension Collection {
+    /// Returns a new `EventLoopFuture` that succeeds with all of the collected
+    /// results once every future in this collection has succeeded, or fails as
+    /// soon as any of them fails.
+    internal func flatten<Value>(on eventLoop: EventLoop) -> EventLoopFuture<[Value]>
+    where Element == EventLoopFuture<Value> {
+        EventLoopFuture.whenAllSucceed(Array(self), on: eventLoop)
+    }
+}
+
+extension Collection where Element == EventLoopFuture<Void> {
+    /// Returns a new `EventLoopFuture<Void>` that succeeds once every future in
+    /// this collection has succeeded, or fails as soon as any of them fails.
+    internal func flatten(on eventLoop: EventLoop) -> EventLoopFuture<Void> {
+        EventLoopFuture.andAllSucceed(Array(self), on: eventLoop)
+    }
+}
